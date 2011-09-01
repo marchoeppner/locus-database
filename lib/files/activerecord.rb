9a1cb38e3fb2e6d0ef410c47ec945cf8c9b6f284
Marc Hoeppner <mh@imbim090.imbim.uu.se>
2011-09-01 09:31:01 +0200

    added dollo support for nodes

--- lib/files/activerecord.rb
+++ lib/files/activerecord.rb
@@ -6,7 +6,7 @@ module LocusDB
     set_primary_key 'genome_db_id'
     has_many :genes
     has_many :xref_genome_datasets
-    has_many :datasets, :through => :xref_genome_datasets
+    #has_many :datasets, :through => :xref_genome_datasets
     
   end # genome_db
   
@@ -14,16 +14,54 @@ module LocusDB
     set_primary_key 'id'
     belongs_to :genome_db, :foreign_key => 'genome_db_id'
     has_many :xref_gene_groupings
-    has_many :groupings, :through => :xref_gene_groupings
+    #has_many :groupings, :through => :xref_gene_groupings
     
   end # gene
   
   class Grouping < DBConnection
     set_primary_key 'id'
     has_many :xref_gene_groupings
-    has_many :genes, :through => :xref_gene_groupings
+    has_many :xref_dollo_groupings
+    #has_many :genes, :through => :xref_gene_groupings
     
+    def state_at_node?(node) 				
+        node = LocusDB::DolloNode.find_by_node(node) unless node.kind_of?(LocusDB::DolloNode)            
+        state = self.xref_dollo_groupings.select{|x| x.dollo_node_id == node.id }.shift.state 				
+        if state == "." and node.node.include?("root")
+          return "0"
+        elsif state == "."
+          self.state_at_node?(node.get_parent)
+        else
+          return state
+        end 			
+  	end
+  	
+  	def deepest_node
+        nodes = self.xref_dollo_groupings.collect{|x| x.dollo_node}.sort_by{|n| n.id}
+        nodes.each do |node|
+          return node if self.state_at_node?(node) == "1"
+        end
+        return nil
+    end
+    
+    def deepest_node_name
+        deepest_node = self.deepest_node
+        if deepest_node.nil?
+          return "Species-specific"
+        else
+          return deepest_node.description
+        end
+    end
+      
   end # grouping
+  
+  class XrefDolloGrouping < DBConnection
+  
+  	set_primary_ids :dollo_node_id,:grouping_id
+  	belongs_to :dollo_node, :foreign_key => "dollo_node_id"
+  	belongs_to :grouping, :foreign_key => "grouping_id"
+  	
+  end
   
   class XrefGeneGrouping < DBConnection
     set_primary_keys :gene_id, :grouping_id
@@ -38,8 +76,7 @@ module LocusDB
     has_many :genomic_align_blocks
     has_many :dollo_nodes
     has_many :xref_genome_datasets
-    
-    
+   
     def genome_dbs
     	return self.xref_genome_datasets.collect{|x| x.genome_db }
     end
@@ -48,14 +85,16 @@ module LocusDB
     	tree_string = self.tree_string
         tree_organisms = self.tree.leaves.collect{|t| t.name.strip }
         tree_organisms.each do |t_o|
-          phylip_name = t_o.gsub(/\s/, '_').phylip_name
-          t_o = t_o.gsub(/\s/ , '_')
-          tree_string.gsub!(/#{t_o}/, "#{phylip_name}")
+      		phylip_name = t_o.gsub(/\s/, '_')[0..9].capitalize
+          	t_o.gsub!(/\s/ , '_')
+          	tree_string.gsub!(/#{t_o}/, "#{phylip_name}")
         end
         return tree_string
-   
     end
     
+   	def tree
+    	return Bio::Newick.new(self.tree_string).tree
+    end
     
   end # dataset
   
@@ -69,12 +108,18 @@ module LocusDB
     set_primary_key 'genomic_align_block_id'
     belongs_to :dataset, :foreign_key => "dataset_id"
     has_many :groupings 
+    
      
   end # genomic_align_block
   
   class DolloNode < DBConnection
     set_primary_keys :dollo_id, :dataset_id
+    has_many :xref_dollo_groupings
     
+    def get_parent
+        return nil if self.child_of.nil?
+        return LocusDB::DolloNode.find(self.child_of)
+    end
   end # dollo_node
   
 end # module
--- lib/locus-database.rb
+++ lib/locus-database.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'bio'
 require 'active_record'
 require 'composite_primary_keys'
 
